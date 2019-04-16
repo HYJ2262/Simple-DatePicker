6c9911adc1eb7c62c19d2ad55c93554f1bcde6db
D7703_24 <d7703_24@d7703-24ui-imac.local>
2019-04-16 15:42:33 +0900

    new test datePicker language and mode

--- Simpe DatePicker/ViewController.swift
+++ Simpe DatePicker/ViewController.swift
@@ -17,21 +17,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // UI의 지역화(locatization)
-        let locale = Locale(identifier: "ko_KO")
+        // UI의 지역화(locatization) -> ko_KO
+        let locale = Locale(identifier: "ja_JA")
         myDatePicker.locale = locale
         
-        // DatePicker 모드 설종
-        myDatePicker.datePickerMode = UIDatePicker.Mode.date
+        // DatePicker 모드 설정
+        myDatePicker.datePickerMode = UIDatePicker.Mode.time
     }
 
     @IBAction func changeDatePicker(_ sender: Any) {
+        //myDatePicker.date 이 데이트 부분을 countdown 이라던가 여러가지 있음
         let selectedData = myDatePicker.date
         print(selectedData)
         
         // Date 형을 문자형으로 변환
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        //formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        formatter.dateFormat = "a HH:mm"
         outLabel.text = formatter.string(from: selectedData)
     }
 }
